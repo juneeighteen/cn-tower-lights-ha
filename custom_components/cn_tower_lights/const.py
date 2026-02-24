76d1a86f39c903d9e2b87783c39454c15b69dc9d
Drew Easley <drew@easleyowl.com>
2026-02-24 17:19:14 -0500

    added missing consts

--- custom_components/cn_tower_lights/const.py
+++ custom_components/cn_tower_lights/const.py
@@ -1,6 +1,7 @@
 DOMAIN = "cn_tower_lights"
 CONF_UPDATE_HOUR = "update_hour"
-
+CONF_NAME = "name"           # <-- ADD THIS!
+DEFAULT_NAME = "CN Tower Lights"  # <-- ADD THIS!
 DEFAULT_UPDATE_HOUR = 2
 
 SENSOR_TODAY = "sensor_today"
